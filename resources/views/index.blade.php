f595a9667abf52452758e5581dd4e6b3403bd25c
AjCruzat89 <ajcruzat89@gmail.com>
2023-06-18 15:43:25 +0800

    Better table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,27 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- Link BOOTSTRAP 5.3.0 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
     <form method="GET" action="logout">
         @csrf
-        <button name="logout">Logout</button>
+        <button name="logout" class="btn btn-primary">Logout</button>
     </form>
 
-    <table>
-        <thead>
+    <table class="table">
+        <thead class="thead-dark">
             <tr>
-                <th>ID</th>
-                <th>Name</th>
+                <th scope="col">id</th>
+                <th scope="col">Username</th>
+                <th scope="col">Password</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($users as $user)
             <tr>
-                <td>{{ $user->id }}</td>
+                <th scope="row">{{ $user->id }}</td>
                 <td>{{ $user->username }}</td>
-                <td>{{ md5($user->username) }}</td>
+                <td>{{ $user->password }}</td>
             </tr>
             @endforeach
         </tbody>
